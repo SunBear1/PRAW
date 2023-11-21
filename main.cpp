293e82701900d3fda5a1f5823f9532cfd03d4e33
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-11-21 19:10:07 +0100

    final version of merge sort

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <omp.h>
 #include <chrono>
+#include <algorithm>
 
 using namespace std::chrono;
 using namespace std;
@@ -67,21 +68,29 @@ void mergeSortOpenMP(vector<int>& vec, int left, int right) {
 
 int main() {
 
-    int number_of_runs = 5;
+    int number_of_runs = 10;
+    int number_of_threads = 1;
+    int number_of_elements = 10000000;
 
     omp_set_dynamic(0);
-    omp_set_num_threads(1);
+    omp_set_num_threads(number_of_threads);
 
     vector<double> runs;
 
+    cout << "Number of threads: " << number_of_threads << endl;
+    cout << "Number of elements: " << number_of_elements << endl;
     for (int i = 0; i < number_of_runs; ++i) {
-        int number_of_elements = 10000000;
+        vector<int> vec = generate_random_vector(number_of_elements, 10000001);
+//        cout << "Sample of the vector: ";
+//        for (int i = 0; i < 10; ++i) {
+//            cout << vec[i] << " ";
+//        }
+//        cout << endl;
+
         auto start = high_resolution_clock::now();
-
-        vector<int> vec = generate_random_vector(number_of_elements, 10000000);
         mergeSortOpenMP(vec, 0, number_of_elements - 1);
-
         auto stop = high_resolution_clock::now();
+
         auto duration = duration_cast<microseconds>(stop - start);
         double time = duration.count() / 1000000.0;
         runs.push_back(time);
@@ -91,8 +100,11 @@ int main() {
 
     for (int i = 0; i < number_of_runs; ++i) {
         final_time += runs[i];
+        cout << "Run " << i + 1 << " time: " << runs[i] << endl;
     }
-    cout << endl << "Sorting time: " << final_time/number_of_runs << endl;
-
+    cout << endl << "Mean time: " << final_time/number_of_runs << endl;
+    vector<double> sorted_runs = runs;
+    sort(sorted_runs.begin(), sorted_runs.end());
+    cout << "Uncertainty: " << (sorted_runs[sorted_runs.size() - 1] - sorted_runs[0])/2 << endl;
     return 0;
 }
